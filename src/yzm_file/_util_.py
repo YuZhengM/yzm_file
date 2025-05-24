46760891e858c9d1ed10c8b930e172a84c26b1dd
yuzhengmin <3236170161@qq.com>
2025-05-24 14:09:11 +0800

    chore: 更新到 0.0.2

--- src/yzm_file/_util_.py
+++ src/yzm_file/_util_.py
@@ -1,13 +1,9 @@
 # -*- coding: UTF-8 -*-
 
 from pathlib import Path
-from typing import Optional, TypeVar, Union, Tuple
+from typing import Union, Tuple
 
 from numpy import ndarray
 
 path = Union[str, Path]
 collection = Union[list, set, Tuple, ndarray]
-
-str_list = Optional[Union[str, list]]
-
-T = TypeVar('T')
